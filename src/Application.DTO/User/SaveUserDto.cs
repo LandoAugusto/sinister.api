e7f41789bbf40130959e387ff123fa5ec28dc073
Leandro Augusto <lando_augusto@hotmail.com>
2023-01-09 09:21:20 -0300

    Autenticação de usuarios.

---
+++ src/Application.DTO/User/SaveUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTO.User
+{
+    public class SaveUserDto
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }        
+        public string Password { get; set; }
+    }
+}
